6aba23d1344a0ba0228549ede32bc40221581612
xiedacon <xiedacon@qq.com>
2018-10-31 13:33:08 +0800

    fix: t.plan

--- lib/test/t.lua
+++ lib/test/t.lua
@@ -29,56 +29,56 @@ end
 
 function T:pass(msg)
     luaunit.success()
-    if self.i ~= 0 then self.i = self.i - 1 end
+    if self.total ~= 0 then self.i = self.i - 1 end
 end
 
 function T:fail(msg)
     luaunit.fail(msg or "")
-    if self.i ~= 0 then self.i = self.i - 1 end
+    if self.total ~= 0 then self.i = self.i - 1 end
 end
 
 function T:truthy(a, msg)
     luaunit.assertEvalToTrue(a, msg or "")
-    if self.i ~= 0 then self.i = self.i - 1 end
+    if self.total ~= 0 then self.i = self.i - 1 end
 end
 
 function T:falsy(a, msg)
     luaunit.assertEvalToFalse(a, msg or "")
-    if self.i ~= 0 then self.i = self.i - 1 end
+    if self.total ~= 0 then self.i = self.i - 1 end
 end
 
 function T:assertTrue(a, msg)
     luaunit.assertTrue(a, msg or "")
-    if self.i ~= 0 then self.i = self.i - 1 end
+    if self.total ~= 0 then self.i = self.i - 1 end
 end
 
 function T:assertFalse(a, msg)
     luaunit.assertFalse(a, msg or "")
-    if self.i ~= 0 then self.i = self.i - 1 end
+    if self.total ~= 0 then self.i = self.i - 1 end
 end
 
 function T:assertIs(a, b, msg)
     if a ~= b then luaunit.fail(msg or "") end
 
-    if self.i ~= 0 then self.i = self.i - 1 end
+    if self.total ~= 0 then self.i = self.i - 1 end
 end
 
 function T:assertNot(a, b, msg)
     if a == b then luaunit.fail(msg or "") end
 
-    if self.i ~= 0 then self.i = self.i - 1 end
+    if self.total ~= 0 then self.i = self.i - 1 end
 end
 
 function T:deepEqual(a, b, msg)
     luaunit.assertEquals(a, b, msg or "")
 
-    if self.i ~= 0 then self.i = self.i - 1 end
+    if self.total ~= 0 then self.i = self.i - 1 end
 end
 
 function T:notDeepEqual(a, b, msg)
     luaunit.assertNotEquals(a, b, msg or "")
 
-    if self.i ~= 0 then self.i = self.i - 1 end
+    if self.total ~= 0 then self.i = self.i - 1 end
 end
 
 return T
